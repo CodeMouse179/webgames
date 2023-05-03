64199148720a4495e3b3409e88061502dbfd67f9
CodeMouse179 <codemouse179@gmail.com>
2023-05-03 19:50:54 +0300

    add index.js, style.css

---
+++ walls/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let canvas = document.getElementById("game_canvas");
+let context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
